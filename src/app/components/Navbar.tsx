6827309613fbad0a6d59c1aedf1b068327a43964
WoraponK <worapon.klabsri@gmail.com>
2024-02-29 00:08:53 +0700

    fix:change about page to github link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <nav className='flex justify-between items-center h-[80px] px-16 border-b border-stone-200'>
+    <nav className='flex justify-between items-center py-4 px-16 border-b border-stone-200 max-md:justify-center max-md:flex-col max-md:space-y-4'>
         <Link href={'/'}>
           <h1 className='text-3xl font-bold'>app-todo</h1>
         </Link>
-        <ul className='flex space-x-4 text-xl'>
+        <ul className='flex space-x-6 text-xl'>
             <li className='hover:underline'><Link href={'/'}>Home</Link></li>
-            <li className='hover:underline'><Link href={'/about'}>About</Link></li>
+            <li className='hover:underline'><a href="https://github.com/WoraponK/todo-local-next">GitHub</a></li>
         </ul>
     </nav>
   )
